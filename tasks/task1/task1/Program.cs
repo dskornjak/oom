37ed5edf5921b60566a6175f7ebd0badca340766
Dinko Skornjak <ic16b003@technikum-wien.at>
2017-04-25 18:37:09 +0200

    add project task1

---
+++ tasks/task1/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            return 0;
+        }
+    }
+}
